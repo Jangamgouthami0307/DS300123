c5432991ac9b9e39ca5dc9b2264326af79555c3c
Jangamgouthami0307 <124508071+Jangamgouthami0307@users.noreply.github.com>
2023-02-20 23:39:07 +0530

    Add files via upload

---
+++ Problem 2 Assignment python.py
@@ -0,0 +1,9 @@
+entered_string = input("Enter a string : ")
+str = ''
+for i in entered_string:
+    str = i + str
+
+print("The revered string is : ", str)  
+
+Enter a string is : Edyoda
+The revered string is : adoydE
